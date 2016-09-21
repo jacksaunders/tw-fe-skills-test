5b0ddb9892ed9c1a1ac0cac35e0ff2fd4de169d0
Nathan Hensher <nhensh@gmail.com>
2016-09-21 16:03:20 +0100

    initial commit

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -15,7 +15,7 @@
   });
 
   $(document).ready(function() {
-    alert("works")
+  	
   });
 
 })(jQuery, window, document);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,7 +7,7 @@
   });
 
   $(document).ready(function() {
-    alert("works")
+  	
   });
 
 })(jQuery, window, document);
